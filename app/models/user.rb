a397e14baefeffac2933eb4de39012fbf547876d
HungNT <ngotienhung2000@gmail.com>
2023-08-29 01:45:34 +0700

    Chapter_9
    Chapter_8
    
    Chapter_9

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,6 +9,35 @@ class User < ApplicationRecord
   has_secure_password
 
   before_save :down_case
+  attr_accessor :remember_token
+
+  class << self
+    def digest string
+      cost = if ActiveModel::SecurePassword.min_cost
+               BCrypt::Engine::MIN_COST
+             else
+               BCrypt::Engine.cost
+             end
+      BCrypt::Password.create string, cost:
+    end
+
+    def new_token
+      SecureRandom.urlsafe_base64
+    end
+  end
+
+  def remember
+    self.remember_token = User.new_token
+    update_column :remember_digest, User.digest(remember_token)
+  end
+
+  def forget
+    update_column :remember_digest, nil
+  end
+
+  def authenticated? remember_token
+    BCrypt::Password.new(remember_digest).is_password? remember_token
+  end
 
   def self.digest string
     cost = if ActiveModel::SecurePassword.min_cost
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_08_28_085439) do
+ActiveRecord::Schema[7.0].define(version: 2023_08_30_094158) do
   create_table "products", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
@@ -23,6 +23,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_28_085439) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "password_digest"
+    t.string "remember_digest"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
