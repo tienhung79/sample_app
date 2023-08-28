60023f915d2f955d3d58d9d13ff39d674c9475fa
HungNT <ngotienhung2000@gmail.com>
2023-08-29 01:45:34 +0700

    Chapter_11
    Chapter_8
    
    Chapter_9
    
    Chapter_10
    
    Chapter_10
    
    Chapter_11

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_08_31_061411) do
+ActiveRecord::Schema[7.0].define(version: 2023_08_31_082957) do
   create_table "products", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
@@ -25,6 +25,9 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_31_061411) do
     t.string "password_digest"
     t.string "remember_digest"
     t.boolean "admin", default: false
+    t.string "activation_digest"
+    t.boolean "activated", default: false
+    t.datetime "activated_at"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
